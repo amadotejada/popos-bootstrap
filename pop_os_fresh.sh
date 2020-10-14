2b89b0e122c23e70ea83c58ba3d6b2cd4c45e9d6
Amado Tejada <tejada.amado@gmail.com>
2020-10-14 01:18:49 -0400

    Update pop_os_fresh.sh

--- pop_os_fresh.sh
+++ pop_os_fresh.sh
@@ -1,11 +1,11 @@
 
-apt update -y &&  apt full-upgrade -y &&  apt autoremove -y &&  apt clean -y &&  apt autoclean -y
-flatpak update
-pop-upgrade release update
-pop-upgrade release upgrade
+sudo apt update -y &&  apt full-upgrade -y &&  apt autoremove -y &&  apt clean -y &&  apt autoclean -y
+sudo flatpak update
+sudo pop-upgrade release update
+sudo pop-upgrade release upgrade
 
 #dependency
-apt install -y gnome-tweaks git
+sudo apt install -y gnome-tweaks git
 
 git clone https://github.com/amadotejada/popos-bootstrap.git ~/macos
 mkdir ~/.themes
@@ -24,24 +24,24 @@ gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgr
 gsettings get org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/macOS-BS1.jpg'
 
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -O ~/vscode-latest.deb
-apt install -y ./vscode-latest.deb
+sudo apt install -y ./vscode-latest.deb
 
 wget https://github.com/Automattic/simplenote-electron/releases/download/v1.21.1/Simplenote-linux-1.21.1-amd64.deb -O ~/simplenote.deb
-apt install -y ./simplenote.deb
+sudo apt install -y ./simplenote.deb
 
 wget https://zoom.us/client/latest/zoom_amd64.deb -O ~/zoom-latest.deb
-apt install -y ./zoom-latest.deb
+sudo apt install -y ./zoom-latest.deb
 
 wget https://addons.mozilla.org/firefox/downloads/latest/chrome-gnome-shell@gnome.org -O ~/chrome-gnome-shell.xpi
 firefox ~/chrome-gnome-shell.xpi
 
-apt install -y vlc slack elegram-desktop mattermost-desktop spotify-client gimp discord caffeine
+sudo apt install -y vlc slack elegram-desktop mattermost-desktop spotify-client gimp discord caffeine
 
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key |  apt-key add -
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' |  tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
-apt update
-apt install albert -y
+sudo apt update
+sudo apt install albert -y
 
 fwupdmgr get-devices
 fwupdmgr get-updates
@@ -49,9 +49,9 @@ fwupdmgr update
 
 firefox https://extensions.gnome.org/extension/307/dash-to-dock/
 
-read -p "Reboot? (y/n): " prompt
-case "$prompt" in
-    y) do sudo reboot now ;;
-    n) do echo bye ;;
-    *) echo "Enter y or n" ;;
-esac
+read -p "Reboot? [Y,n]" input
+if [[ $input == "Y" || $input == "y" ]]; then
+        sudo reboot now
+else
+        echo "bye"
+fi
