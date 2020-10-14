65b7d6a6a1cf7fc29f35c408c0bb27314d0ca63d
Amado Tejada <tejada.amado@gmail.com>
2020-10-14 17:31:01 -0400

    Update pop_os_fresh.sh

--- pop_os_fresh.sh
+++ pop_os_fresh.sh
@@ -3,6 +3,7 @@
 gsettings set com.system76.hidpi enable false
 xrandr --output eDP-1 --mode 1920x1080
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+killall -3 gnome-shell
 
 #apt/flatpak updates
 sudo apt update -y &&  sudo apt full-upgrade -y
@@ -22,19 +23,17 @@ cp -rf ~/macos/icons/* ~/.icons
 cp -rf ~/macos/wallpapers/* /usr/share/backgrounds/
 
 #set theme
+gsettings set org.gnome.desktop.interface cursor-size 48
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/tony-webster-97532.jpg'
 gsettings set org.gnome.mutter center-new-windows 'true'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
 gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark"
 gsettings set org.gnome.desktop.wm.preferences theme "WhiteSur-dark"
 gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors-light'
-gsettings set org.gnome.desktop.interface cursor-size 32
 
-gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock animate-show-apps false
-gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 # gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-
 # gsettings set org.gnome.desktop.interface icon-theme 'BigSur'
-# gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/macOS-BS2.jpg'
 # gsettings get org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/macOS-BS1.jpg'
 
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -O ~/vscode-latest.deb
@@ -45,6 +44,11 @@ sudo apt install -y ./simplenote.deb
 
 wget https://addons.mozilla.org/firefox/downloads/latest/chrome-gnome-shell@gnome.org -O ~/chrome-gnome-shell.xpi
 firefox ~/chrome-gnome-shell.xpi
+
+firefox https://extensions.gnome.org/extension/307/dash-to-dock/
+
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock animate-show-apps false
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 
 sudo flatpak -y install mattermost telegram slack discord app/org.gimp.GIMP/x86_64/stable app/org.videolan.VLC/x86_64/stable us.zoom.Zoom
 #vscode com.visualstudio.code
@@ -59,6 +63,6 @@ fwupdmgr get-devices
 fwupdmgr get-updates
 fwupdmgr update
 
-firefox https://extensions.gnome.org/extension/307/dash-to-dock/
+
 
 echo "Done - Reboot"
