77ba5401bab4234315350f338eeb455259b11d4e
Amado Tejada <tejada.amado@gmail.com>
2020-10-14 20:01:33 -0400

    Update pop_os_fresh.sh

--- pop_os_fresh.sh
+++ pop_os_fresh.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 #screen settings
 gsettings set com.system76.hidpi enable false
@@ -12,21 +13,21 @@ gsettings set org.gnome.desktop.interface enable-animations false
 killall -3 gnome-shell
 
 #apt/flatpak updates
-sudo apt update -y &&  sudo apt full-upgrade -y
-sudo flatpak update
-sudo pop-upgrade release update
-sudo pop-upgrade release upgrade
+apt update -y &&  apt full-upgrade -y
+flatpak update
+pop-upgrade release update
+pop-upgrade release upgrade
 
 #dependency
-sudo apt install -y gnome-tweaks git caffeine
+apt install -y gnome-tweaks git caffeine
 
 #github files
 git clone https://github.com/amadotejada/popos-bootstrap.git ~/macos
 mkdir ~/.themes
 mkdir ~/.icons
-sudo cp -rf ~/macos/themes/* ~/.themes
-sudo cp -rf ~/macos/icons/* ~/.icons
-sudo cp -rf ~/macos/wallpapers/* /usr/share/backgrounds/
+cp -rf ~/macos/themes/* ~/.themes
+cp -rf ~/macos/icons/* ~/.icons
+cp -rf ~/macos/wallpapers/* /usr/share/backgrounds/
 
 #set theme
 gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/pop/tony-webster-97532.jpg'
@@ -37,10 +38,10 @@ gsettings set org.gnome.desktop.wm.preferences theme "WhiteSur-dark"
 gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors-light'
 
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -O ~/vscode-latest.deb
-sudo apt install -y ./vscode-latest.deb
+apt install -y ./vscode-latest.deb
 
 wget https://github.com/Automattic/simplenote-electron/releases/download/v1.21.1/Simplenote-linux-1.21.1-amd64.deb -O ~/simplenote.deb
-sudo apt install -y ./simplenote.deb
+apt install -y ./simplenote.deb
 
 wget https://addons.mozilla.org/firefox/downloads/latest/chrome-gnome-shell@gnome.org -O ~/chrome-gnome-shell.xpi
 firefox ~/chrome-gnome-shell.xpi
@@ -52,26 +53,26 @@ gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.
 
 #setup autologin
 hello=$(whoami)
-sudo sed -i 's/#  AutomaticLoginEnable = true[^ ]*/AutomaticLoginEnable=True/' /etc/gdm3/custom.conf
-sudo sed -i 's/#  AutomaticLogin = user1[^ ]*/AutomaticLogin='"$hello"'/' /etc/gdm3/custom.conf
+sed -i 's/#  AutomaticLoginEnable = true[^ ]*/AutomaticLoginEnable=True/' /etc/gdm3/custom.conf
+sed -i 's/#  AutomaticLogin = user1[^ ]*/AutomaticLogin='"$hello"'/' /etc/gdm3/custom.conf
 
 #check/update firmwares
 fwupdmgr get-devices
 fwupdmgr get-updates
 fwupdmgr update
 
-sudo reboot
+reboot
 
 '''future'''
 # gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 # gsettings set org.gnome.desktop.interface icon-theme 'BigSur'
 # gsettings get org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/macOS-BS1.jpg'
 
-# sudo flatpak -y install mattermost telegram slack discord app/org.gimp.GIMP/x86_64/stable app/org.videolan.VLC/x86_64/stable us.zoom.Zoom
+# flatpak -y install mattermost telegram slack discord app/org.gimp.GIMP/x86_64/stable app/org.videolan.VLC/x86_64/stable us.zoom.Zoom
 #vscode com.visualstudio.code
 
 # curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key |  apt-key add -
 # echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' |  tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 # wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
-# sudo apt update
-# sudo apt install albert -y
+# apt update
+# apt install albert -y
