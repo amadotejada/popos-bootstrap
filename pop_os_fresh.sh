2be0982638e93607a7352643fb93ec13f5328c6c
Amado Tejada <tejada.amado@gmail.com>
2020-10-18 01:16:09 -0400

    Update pop_os_fresh.sh

--- pop_os_fresh.sh
+++ pop_os_fresh.sh
@@ -40,45 +40,45 @@ sudo apt install -y gnome-tweaks git cryptomator
 # gsettings set org.gnome.desktop.wm.preferences theme "WhiteSur-dark"
 # gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors-light'
 
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O ~/vscode-latest.deb
-sudo apt install -y ./vscode-latest.deb
-rm vscode-latest.deb
+# wget https://go.microsoft.com/fwlink/?LinkID=760868 -O ~/vscode-latest.deb
+# sudo apt install -y ./vscode-latest.deb
+# rm vscode-latest.deb
 
-wget https://github.com/Automattic/simplenote-electron/releases/download/v1.21.1/Simplenote-linux-1.21.1-amd64.deb -O ~/simplenote.deb
-sudo apt install -y ./simplenote.deb
-rm simplenote.deb
+# wget https://github.com/Automattic/simplenote-electron/releases/download/v1.21.1/Simplenote-linux-1.21.1-amd64.deb -O ~/simplenote.deb
+# sudo apt install -y ./simplenote.deb
+# rm simplenote.deb
 
-wget https://addons.mozilla.org/firefox/downloads/latest/chrome-gnome-shell@gnome.org -O ~/chrome-gnome-shell.xpi
-firefox ~/chrome-gnome-shell.xpi
-rm chrome-gnome-shell.xpi
+# wget https://addons.mozilla.org/firefox/downloads/latest/chrome-gnome-shell@gnome.org -O ~/chrome-gnome-shell.xpi
+# firefox ~/chrome-gnome-shell.xpi
+# rm chrome-gnome-shell.xpi
 
-firefox https://extensions.gnome.org/extension/307/dash-to-dock/ https://extensions.gnome.org/extension/277/impatience/ https://extensions.gnome.org/extension/517/caffeine/ https://extensions.gnome.org/extension/945/cpu-power-manager/ https://extensions.gnome.org/extension/779/clipboard-indicator/ https://extensions.gnome.org/extension/1036/extensions/ https://extensions.gnome.org/extension/442/drop-down-terminal/ https://extensions.gnome.org/extension/760/minimize-all/ 
+# firefox https://extensions.gnome.org/extension/307/dash-to-dock/ https://extensions.gnome.org/extension/277/impatience/ https://extensions.gnome.org/extension/517/caffeine/ https://extensions.gnome.org/extension/945/cpu-power-manager/ https://extensions.gnome.org/extension/779/clipboard-indicator/ https://extensions.gnome.org/extension/1036/extensions/ https://extensions.gnome.org/extension/442/drop-down-terminal/ https://extensions.gnome.org/extension/760/minimize-all/ 
 
-gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock animate-show-apps false
-gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+# gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock animate-show-apps false
+# gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 
-#expressvpn
-wget https://www.expressvpn.com/expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
-gpg --import expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
-rm expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
-wget https://www.expressvpn.works/clients/linux/expressvpn_3.1.0.34-1_amd64.deb -O ~/expressvpn.deb
-sudo apt install -y ./expressvpn.deb
-rm expressvpn.deb
+# #expressvpn
+# wget https://www.expressvpn.com/expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
+# gpg --import expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
+# rm expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
+# wget https://www.expressvpn.works/clients/linux/expressvpn_3.1.0.34-1_amd64.deb -O ~/expressvpn.deb
+# sudo apt install -y ./expressvpn.deb
+# rm expressvpn.deb
 
-#setup autologin
-hello=$(whoami)
-sudo sed -i 's/#  AutomaticLoginEnable = true[^ ]*/AutomaticLoginEnable=True/' /etc/gdm3/custom.conf
-sudo sed -i 's/#  AutomaticLogin = user1[^ ]*/AutomaticLogin='"$hello"'/' /etc/gdm3/custom.conf
+# #setup autologin
+# hello=$(whoami)
+# sudo sed -i 's/#  AutomaticLoginEnable = true[^ ]*/AutomaticLoginEnable=True/' /etc/gdm3/custom.conf
+# sudo sed -i 's/#  AutomaticLogin = user1[^ ]*/AutomaticLogin='"$hello"'/' /etc/gdm3/custom.conf
 
-#install flatpaks
-flatpak install -y flathub io.bit3.WhatsAppQT com.obsproject.Studio com.mattermost.Desktop org.telegram com.slack.Slack com.discordapp.Discord org.gimp.GIMP org.videolan.VLC us.zoom.Zoom
+# #install flatpaks
+# flatpak install -y flathub io.bit3.WhatsAppQT com.obsproject.Studio com.mattermost.Desktop org.telegram com.slack.Slack com.discordapp.Discord org.gimp.GIMP org.videolan.VLC us.zoom.Zoom
 
-#check/update firmwares
-fwupdmgr get-devices
-fwupdmgr get-updates
-fwupdmgr update
+# #check/update firmwares
+# fwupdmgr get-devices
+# fwupdmgr get-updates
+# fwupdmgr update
 
-echo "Done - Reboot"
+# echo "Done - Reboot"
 
 #future
 # gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
